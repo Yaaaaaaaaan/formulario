dbec72c1eff91afbc519f13146c08c61fd50b730
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-07-04 08:16:15 -0300

    Add files via upload

---
+++ Nova pasta - Copia/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "formulario-"
+
+
+
+// criar a conexao 
+
+
+$conn = mysqli_connect($servidor,$usuario, $senha, $dbname);
+
+?>
