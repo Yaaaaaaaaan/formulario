b841c9975dc0543a657d5a81140024138c9a88e0
Yan Fonseca <yangoular@gmail.com>
2023-07-08 08:09:14 -0300

    Revert "início da modelagem de dados"
    This reverts commit 8c5ec7eb7ae933da60e46cea283c47b54e54373c.

--- AppForm/projetoNovo/Controllers/AppController.php
+++ AppForm/projetoNovo/Controllers/AppController.php
@@ -17,7 +17,6 @@
             $ctt = $_POST['ctt'];
 
             // Primeira parte já feita. Classe criada e código recebido.
-            //tá retornando erro, analisar com mais calma o que pode ser...
             $usuario = Container::getModel('Usuario');
             $usuario->__set('matr',$_POST['matr']);
             $usuario->__set('email',$_POST['email']);
